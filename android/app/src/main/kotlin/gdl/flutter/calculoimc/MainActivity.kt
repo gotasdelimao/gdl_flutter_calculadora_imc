edf318dab7d9357763f3a9ca17a2c4f0bc895a46
Gotas de Limão <murilo@gotasdelimao.com.br>
2020-05-23 16:17:52 -0300

    App Calculadora de IMC

---
+++ android/app/src/main/kotlin/gdl/flutter/calculoimc/MainActivity.kt
@@ -0,0 +1,6 @@
+package gdl.flutter.calculoimc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GDL Flutter App',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Calculadora de IMC'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  TextEditingController weightEC = TextEditingController();
+  TextEditingController heightEC = TextEditingController();
+  String _infoText = "Informe seus dados!";
+
+  void _resetFields() {
+    weightEC.text = "";
+    heightEC.text = "";
+    _infoText = "Informe seus dados!";
+  }
+
+  void calculate() {
+    setState(() {
+      double weight = double.parse(weightEC.text);
+      double height = double.parse(heightEC.text) / 100;
+      double imc = weight / (height * height);
+      if (imc < 18.6) {
+        _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso Ideal (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente Acima do Peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 40.0) {
+        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(3)})";
+      }
+    });
+  }
+
+  /*
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+  */
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.green,
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: _resetFields,
+          ),
+        ],
+        ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView( //permite que a tela role sob o teclado
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.00, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person_outline,
+                size: 120.0,
+                color: Colors.green
+            ),
+            TextField( //PESO
+              controller: weightEC,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Peso (kg)",
+                labelStyle: TextStyle(color: Colors.green),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.green,
+                fontSize: 25.0,
+              ),
+            ),
+            TextField( //ALTURA
+              controller: heightEC,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Altura (cm)",
+                labelStyle: TextStyle(color: Colors.green),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.green,
+                fontSize: 25.0,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                        top: 10.0,
+                        bottom: 10.0,
+              ),
+              child: Container(
+                height: 50.0,
+                child: RaisedButton(
+                  onPressed: calculate,
+                  child: Text("Calcular",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.0),
+                  ),
+                  color: Colors.green,
+                ),
+              ),
+            ),
+            Text("$_infoText",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.green,
+                fontSize: 25.00,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
